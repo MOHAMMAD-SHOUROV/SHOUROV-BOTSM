83b9b0636dcf7a6b227148345f31537a2776fba0
ALIHSAN-SHOUROV <164044359+MOHAMMAD-SHOUROV@users.noreply.github.com>
2026-01-14 17:50:03 +0600

    Update love.js

--- scripts/cmds/love.js
+++ scripts/cmds/love.js
@@ -1,16 +1,14 @@
 const jimp = require("jimp");
-const fs = require("fs-extra");
+const fs = require("fs");
 
 module.exports = {
   config: {
     name: "love",
-    aliases: ["love2"],
     role: 0,
-    category: "fun",
-    shortDescription: "Love photo"
+    category: "photo"
   },
 
-  onStart: async function ({ api,message,event,args }) {
+  onStart: async function ({ api, event }) {
     const mention = Object.keys(event.mentions);
 
     if (mention.length === 0)
@@ -20,50 +18,41 @@ module.exports = {
         event.messageID
       );
 
+    const one = event.senderID;
+    const two = mention[0];
 
-      const one = event.senderID;
-      const two = mention[0];
-
-      const path = await makeLove(one, two);
+    try {
+      const imgPath = await makeLove(one, two);
 
       api.sendMessage(
         {
-          body: "ইগো আর ভালোবাসা লড়াই করলে ভালোবাসাই হেরে যায় 💔🥀",
-          attachment: fs.createReadStream(path)
+          body: "ইগো আর ভালোবাসা লড়াই হলে ভালোবাসা টাই হেরে যায় 💔🥀",
+          attachment: fs.createReadStream(imgPath)
         },
         event.threadID,
-        () => fs.unlinkSync(path),
+        () => fs.unlinkSync(imgPath),
         event.messageID
       );
-
     } catch (e) {
-      api.sendMessage(
-        "❌ Love image তৈরি করা যায়নি",
-        event.threadID,
-        event.messageID
-      );
+      console.error(e);
+      api.sendMessage("❌ Image তৈরি করা যায়নি", event.threadID);
     }
   }
 };
 
 async function makeLove(one, two) {
-  const img = await jimp.read("https://i.imgur.com/LjpG3CW.jpeg");
+  let av1 = await jimp.read(`https://graph.facebook.com/${one}/picture?width=512&height=512`);
+  let av2 = await jimp.read(`https://graph.facebook.com/${two}/picture?width=512&height=512`);
 
-  const av1 = await jimp.read(
-    `https://graph.facebook.com/${one}/picture?width=512&height=512`
-  );
-  const av2 = await jimp.read(
-    `https://graph.facebook.com/${two}/picture?width=512&height=512`
-  );
+  av1.circle();
+  av2.circle();
 
-  av1.circle().resize(350, 350);
-  av2.circle().resize(350, 350);
+  let img = await jimp.read("https://i.imgur.com/LjpG3CW.jpeg");
+  img.resize(1440, 1080)
+     .composite(av1.resize(470, 470), 125, 210)
+     .composite(av2.resize(470, 470), 800, 200);
 
-  img.resize(1200, 800)
-    .composite(av1, 100, 200)
-    .composite(av2, 700, 200);
-
-  const path = "love.png";
+  const path = `love_${one}_${two}.png`;
   await img.writeAsync(path);
   return path;
 }
