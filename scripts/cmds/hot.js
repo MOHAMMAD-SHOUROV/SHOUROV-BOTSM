6bc01e8e99b7c32e44bd7b316cf4f736a17e9992
ALIHSAN-SHOUROV <164044359+MOHAMMAD-SHOUROV@users.noreply.github.com>
2026-01-20 11:39:39 -0800

    Update hot.js

--- scripts/cmds/hot.js
+++ scripts/cmds/hot.js
@@ -1,51 +1,32 @@
+const axios = require("axios");
+
 module.exports = {
   config: {
     name: "hot",
-    version: "7.1",
-    author: "Alihsan Shourov",
-    countDown: 15,
+    version: "8.0",
+    author: "alihsan shourov",
+    countDown: 10,
     role: 0,
-    shortDescription: "all video",
-    longDescription: "",
-    category: "video",
-    guide: "{pn}"
+    category: "Video",
+    shortDescription: "Random video from API"
   },
 
-  onStart: async function ({ api, event }) {
-    const videos = [
-      "https://i.imgur.com/aQVa9EL.mp4",
-      "https://i.imgur.com/wzR3OP7.mp4",
-      "https://i.imgur.com/AaPoSEo.mp4",
-      "https://i.imgur.com/zeqzgYJ.mp4",
-      "https://i.imgur.com/tfePTdM.mp4",
-      "https://i.imgur.com/FVtCcS4.mp4",
-      "https://i.imgur.com/MwiTEUL.mp4",
-      "https://i.imgur.com/ka0pxxO.mp4",
-      "https://i.imgur.com/oBcryzJ.mp4",
-      "https://i.imgur.com/vfYOmHS.mp4",
-      "https://i.imgur.com/HOSrfId.mp4",
-      "https://i.imgur.com/xIi5ZjB.mp4",
-      "https://i.imgur.com/6vGHjRM.mp4",
-      "https://i.imgur.com/08yfKpb.mp4",
-      "https://i.imgur.com/deSrgBg.mp4",
-      "https://i.imgur.com/vLcyKJ2.mp4",
-      "https://i.imgur.com/uVBK5gc.mp4",
-      "https://i.imgur.com/bFd7QRW.mp4",
-      "https://i.imgur.com/yIViust.mp4",
-      "https://i.imgur.com/GTxZZfN.mp4",
-      "https://i.imgur.com/Nu5DcgN.mp4",
-      "https://i.imgur.com/zSse6lu.mp4"
-    ];
+  onStart: async function ({ message }) {
+    try {
+      // 🔗 তোমার API link
+      const apiUrl = "https://shourov-api.vercel.app/api/hot";
 
-    const randomVideo = videos[Math.floor(Math.random() * videos.length)];
+      const res = await axios.get(apiUrl);
+      const videoUrl = res.data.video;
 
-    api.sendMessage({
-      body:
-        " লুচ্চা নে দিলাম\n" +
-        "uffs💋\n\n" +
-        "uffs jal💋🥵" +
-        "Video credit : Alihsan Shourov",
-      attachment: await global.utils.getStreamFromURL(randomVideo)
-    }, event.threadID, event.messageID);
+      message.send({
+        body: "🔥 Random video from API",
+        attachment: await global.utils.getStreamFromURL(videoUrl)
+      });
+
+    } catch (err) {
+      console.error(err);
+      message.send("⚠️ Video আনতে সমস্যা হয়েছে");
+    }
   }
 };
