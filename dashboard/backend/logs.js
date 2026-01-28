ff06d07f2a739a1eb11e200822d475b1789bdf38
ALIHSAN-SHOUROV <164044359+MOHAMMAD-SHOUROV@users.noreply.github.com>
2026-01-29 02:50:53 +0600

    Delete logs.js

--- dashboard/backend/logs.js
+++
@@ -1,8 +0,0 @@
-const { exec } = require("child_process");
-
-module.exports.logs = (req, res) => {
-  exec("pm2 logs shourov-bot-main --lines 50", (err, stdout) => {
-    if (err) return res.send(err.message);
-    res.send(stdout);
-  });
-};
