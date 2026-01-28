c95bfbaf56e7794744217b7304a95b284492312f
ALIHSAN-SHOUROV <164044359+MOHAMMAD-SHOUROV@users.noreply.github.com>
2026-01-29 02:12:13 +0600

    Delete bot.js

--- dashboard/api/bot.js
+++
@@ -1,5 +0,0 @@
-const { exec } = require("child_process");
-
-exports.start = () => exec("pm2 start shourov-bot");
-exports.stop = () => exec("pm2 stop shourov-bot");
-exports.restart = () => exec("pm2 restart shourov-bot");
